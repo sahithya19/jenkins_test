537592b59393ae0dac5b612d6969964b4fcc7559
sahithya19 <sahithyav@biarca.com>
2019-05-23 16:39:55 +0530

    resolved assertion error

--- test1.py
+++ test1.py
@@ -6,7 +6,7 @@ class SimpleTest(unittest.TestCase):
     # Returns True or False.  
     def test_div(self):
         a = div(5,2)
-        self.assertEqual(2.5,a)
+        self.assertEqual(2,a)
 
     def test_sub(self):
         a = sub(5,2)
